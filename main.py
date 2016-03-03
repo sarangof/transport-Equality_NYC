00506c6929749f511be91c28a06d1a2d135fe531
Sara Arango <sarangofc@gmail.com>
2016-03-02 19:26:38 -0500

    setting up structure

--- main.py
+++ main.py
@@ -11,12 +11,34 @@ from mpl_toolkits.basemap import Basemap
 
 sf = shp.Reader("data/nycb2010.shp")
 
-map = Basemap(llcrnrlon=-0.5,llcrnrlat=39.8,urcrnrlon=4.,urcrnrlat=43.,
-             resolution='i', projection='tmerc', lat_0 = 39.5, lon_0 = 1)
+#map = Basemap(llcrnrlon=-0.5,llcrnrlat=39.8,urcrnrlon=4.,urcrnrlat=43.,
+#             resolution='i', projection='tmerc', lat_0 = 39.5, lon_0 = 1)
+#
+#map.drawmapboundary(fill_color='aqua')
+#map.fillcontinents(color='#ddaa66',lake_color='aqua')
+#map.drawcoastlines()
+#map.readshapefile("data/nycb2010.shp",'nycb2010')
+#
+#plt.show()
 
-map.drawmapboundary(fill_color='aqua')
-map.fillcontinents(color='#ddaa66',lake_color='aqua')
-map.drawcoastlines()
-map.readshapefile("data/nycb2010.shp",'nycb2010')
 
-plt.show()
+
+""" Initial step: build number of people that travel between block groups by mode.
+"""
+
+# Load number of people travelling between block groups.
+
+
+# Load modes per people, transform to frequency
+
+"""
+Component A - transportation expenses versus income
+"""
+
+"""
+# Component B - time versus distance
+"""
+
+"""
+# Component C - Accesibility
+"""
