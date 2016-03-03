eaa09f17de1bbbbd7c84ab394e3f33239bec1eb9
Sara Arango <sarangofc@gmail.com>
2016-03-03 10:45:18 -0500

    Finally reading and subsetting LODES data

--- readOD.py
+++ readOD.py
@@ -5,29 +5,22 @@ Created on Wed Mar  2 19:58:54 2016
 @author: saf537
 """
 
-import shapefile as shp
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-#import geopandas as gdp
-import pylab as pl
 import pandas as pd
-import numpy as np
-import scipy.stats
-import statsmodels.api as sm
-import glob
-import requests
-import io
+import urllib2
+from StringIO import StringIO
+import gzip
 
 # get data file names
 dfs = []
-for i in [0,5]:
-    url = 'http://lehd.ces.census.gov/data/lodes/LODES7/ny/od/ny_od_main_JT0'+ str(i) +'_2013.csv.gz'
-    s=requests.get(url).content
-    c=pd.read_csv(io.StringIO(s.decode('utf-8')))
-    dfs.append(c)
-# Concatenate all data into one DataFrame
-
-big_frame = pd.concat(dfs, ignore_index=True)
-
-sf = shp.Reader("data/nycb2010.shp")
-#
+for i in [0,1]:
+    baseURL = 'http://lehd.ces.census.gov/data/lodes/LODES7/ny/od/'
+    filename = 'ny_od_main_JT0'+ str(i) +'_2013.csv.gz'
+    outFilePath = filename[:-3]    
+    response = urllib2.urlopen(baseURL + filename)
+    compressedFile = StringIO(response.read())
+    decompressedFile = gzip.GzipFile(fileobj=compressedFile) 
+    with open(outFilePath,'w') as outfile:
+        outfile.write(decompressedFile.read())
+    df = pd.read_csv(outFilePath)
+    df.drop(df.columns[2:],axis=1)
+print df.head()    
